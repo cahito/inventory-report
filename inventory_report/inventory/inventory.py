227b98ce616a3566cb36e24e0939859b7a058f5b
cahito <cahito@gmail.com>
2022-11-28 16:34:35 -0300

    Feat - Requisito seis cumprido.

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,16 +1,23 @@
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 import csv
+import json
 
 
 class Inventory:
     @classmethod
     def import_data(cls, path, tipo_relatorio):
-        with open(path, encoding='utf-8') as file:
-            relatorio = csv.DictReader(file, delimiter=',', quotechar='"')
-            dados_relatorio = list(relatorio)
+        dados_relatorio = list()
 
-        if tipo_relatorio == 'simples':
+        if path.endswith(".json"):
+            relatorio = json.load(open(path, "r"))
+            dados_relatorio = list(relatorio)
+        else:
+            with open(path, encoding="utf-8") as file:
+                relatorio = csv.DictReader(file, delimiter=",", quotechar='"')
+                dados_relatorio = list(relatorio)
+
+        if tipo_relatorio == "simples":
             return SimpleReport.generate(dados_relatorio)
 
         return CompleteReport.generate(dados_relatorio)
