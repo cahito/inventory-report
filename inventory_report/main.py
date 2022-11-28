a7c30c185aeb777cf9450740df1dffea9e520ecd
cahito <cahito@gmail.com>
2022-11-28 18:00:15 -0300

    Feat - Requisito onze cumprido. Projeto finalizado.

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,27 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.importer.json_importer import JsonImporter
+
+
+def get_tipo_do_importer(path: str):
+    if path.endswith(".csv"):
+        return CsvImporter
+    if path.endswith(".xml"):
+        return XmlImporter
+    if path.endswith(".json"):
+        return JsonImporter
+
+
 def main():
-    pass
+    try:
+        path, tipo_relatorio = sys.argv[1:]
+    except ValueError:
+        print("Verifique os argumentos", file=sys.stderr)
+        return
+
+    importer = get_tipo_do_importer(path)
+    inventory = InventoryRefactor(importer)
+
+    print(inventory.import_data(path, tipo_relatorio), end="")
