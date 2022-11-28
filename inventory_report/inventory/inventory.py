06d4609828573cd9fcee07b61f943375bbfe9ba7
cahito <cahito@gmail.com>
2022-11-28 16:39:30 -0300

    Feat - Requisito sete cumprido. xmltodict instalado e colocado no dev-req.

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 import csv
 import json
+import xmltodict
 
 
 class Inventory:
@@ -12,6 +13,10 @@ class Inventory:
         if path.endswith(".json"):
             relatorio = json.load(open(path, "r"))
             dados_relatorio = list(relatorio)
+        elif path.endswith('.xml'):
+            with open(path) as file:
+                relatorio = xmltodict.parse(file.read())
+                dados_relatorio = relatorio['dataset']['record']
         else:
             with open(path, encoding="utf-8") as file:
                 relatorio = csv.DictReader(file, delimiter=",", quotechar='"')
