16f0d46e692aa6c0cbf2b9fdc4619d8436a18515
cahito <cahito@gmail.com>
2022-11-28 17:16:50 -0300

    Feat - Requisito nove cumprido.

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,41 @@
-# from inventory_report.reports.colored_report import ColoredReport
+import pytest
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
 
 
-def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+@pytest.fixture
+def data():
+    return [
+        {
+            "id": 1,
+            "nome_da_empresa": "FarmaConde",
+            "data_de_fabricacao": "2020-08-12",
+            "data_de_validade": "2023-08-11",
+        },
+        {
+            "id": 2,
+            "nome_da_empresa": "Petybon",
+            "data_de_fabricacao": "2022-09-21",
+            "data_de_validade": "2023-09-21",
+        },
+        {
+            "id": 3,
+            "nome_da_empresa": "Johnson&Johnson",
+            "data_de_fabricacao": "2021-04-05",
+            "data_de_validade": "2024-04-05",
+        },
+    ]
+
+
+def test_decorar_relatorio(data):
+    relatorio_colorido = ColoredReport(SimpleReport)
+    relatorio = relatorio_colorido.generate(data)
+
+    assert "\033[32mData de fabricação mais antiga:\033[0m" in relatorio
+    assert "\033[36m2020-08-12\033[0m" in relatorio
+
+    assert "\033[32mData de validade mais próxima:\033[0m" in relatorio
+    assert "\033[36m2023-08-11\033[0m" in relatorio
+
+    assert "\033[32mEmpresa com mais produtos:\033[0m" in relatorio
+    assert "\033[31mFarmaConde\033[0m" in relatorio
